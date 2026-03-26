da5c48788af31e92b89da5730b599d330be72ef8
thomas-reifer <tom.reifer@gmail.com>
2026-03-26 09:54:11 +0100

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -246,7 +246,7 @@ useHead({
 
             <div class="mt-2 opacity-80">
               <p>Sparkassenstraße 15</p>
-              <p>39100 Südtirol, Italien</p>
+              <p>39100 Bozen (BZ), Italien</p>
             </div>
             <p class="mt-6">
               © 2026 |
