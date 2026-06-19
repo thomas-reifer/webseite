9b8b57cd037fa89e37fb6e957eeeeaeade1a3662
thomas-reifer <tom.reifer@gmail.com>
2026-06-19 15:20:32 +0200

    Create GoogleAdsConversion.client.vue

---
+++ components/GoogleAdsConversion.client.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+onMounted(() => {
+  const w = window as any
+
+  if (typeof w.gtag === 'function') {
+    w.gtag('event', 'ads_conversion_Terminanfrage_abgeschlo_1', {})
+  }
+})
+</script>
+
+<template>
+  <span style="display: none;" aria-hidden="true"></span>
+</template>
